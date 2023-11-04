9f778d4e479bcaf98fb20f7127632d0fcd1a4937
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-11-04 11:38:49 -0700

    updating invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -41,3 +41,30 @@ export async function createInvoice(formData: FormData) {
   revalidatePath('/dashboard/invoices')
   redirect('/dashboard/invoices')
 }
+
+// update
+// Use Zod to update the expected types
+const UpdateInvoice = InvoiceSchema.omit({ date: true, id: true })
+
+// ...
+
+export async function updateInvoice(id: string, formData: FormData) {
+  console.log({ id, formData })
+
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  })
+
+  const amountInCents = amount * 100
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `
+
+  revalidatePath('/dashboard/invoices')
+  redirect('/dashboard/invoices')
+}
