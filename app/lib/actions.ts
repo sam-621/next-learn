5f21b000b6cf7136b5c2b334a772f1dbba5d7525
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-11-04 12:27:43 -0700

    delete invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -68,3 +68,12 @@ export async function updateInvoice(id: string, formData: FormData) {
   revalidatePath('/dashboard/invoices')
   redirect('/dashboard/invoices')
 }
+
+// deleting
+const UpdateInvoiceSchema = InvoiceSchema.omit({ date: true, id: true })
+// ...
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`
+  revalidatePath('/dashboard/invoices')
+}
